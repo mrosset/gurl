4baaebaac348ea0d3ff3ff0391ef85c196f72a78
str1ngs <schizoid29@gmail.com>
2011-05-14 12:52:36 -0700

    progress bar handles terminal width

--- pkg/gurl.go
+++ pkg/gurl.go
@@ -5,20 +5,49 @@ import (
 	"http"
 	"os"
 	"path"
-	term "github.com/kless/go-term/term"
+	//term "github.com/kless/go-term/term"
+	"syscall"
+	"time"
+	"unsafe"
 )
 
 var (
 	printf  = fmt.Printf
+	fprintf = fmt.Fprintf
 	println = fmt.Println
+	debug   = false
+)
+
+const (
+	TIOCGWINSZ = 0x5413
 )
 
 type Client struct {
 	client *http.Client
 }
 
+type winsize struct {
+	Row    uint16
+	Col    uint16
+	Xpixel uint16
+	Ypixel uint16
+}
+
+func getWinSize() (*winsize, os.Error) {
+	ws := new(winsize)
+	r1, _, errno := syscall.Syscall(syscall.SYS_IOCTL,
+		uintptr(syscall.Stdin),
+		uintptr(TIOCGWINSZ),
+		uintptr(unsafe.Pointer(ws)))
+
+	if int(r1) == -1 {
+		return nil, os.NewSyscallError("getWinSize", int(errno))
+	}
+	return ws, nil
+}
+
 func (v *Client) Download(destdir string, url string) os.Error {
-	winsize, err := term.GetWinsize()
+	winsize, err := getWinSize()
 	if err != nil {
 		return err
 	}
@@ -29,17 +58,35 @@ func (v *Client) Download(destdir string, url string) os.Error {
 	if err != nil {
 		return err
 	}
+	if debug {
+		b, err := http.DumpRequest(request, false)
+		if err != nil {
+			return err
+		}
+		os.Stderr.Write(b)
+	}
 	response, err := v.client.Do(request)
 	defer response.Body.Close()
 	if err != nil {
 		return err
 	}
+	if debug {
+		b, err := http.DumpResponse(response, false)
+		if err != nil {
+			return err
+		}
+		os.Stderr.Write(b)
+	}
 	fpath := path.Join(destdir, path.Base(url))
 	f, err := os.Create(fpath)
 	defer f.Close()
-	segments := response.ContentLength / (int64(winsize.Col) - 4)
-
-	var bcount int64
+	_ = response.ContentLength / int64(winsize.Col)
+	var tdown int64
+	start := time.Seconds()
+	start = start
+	println("started", start)
+	//start := time.Seconds()
+	tick := time.NewTicker(1e09)
 	for {
 		b := make([]byte, 1024)
 		read, err := response.Body.Read(b)
@@ -53,15 +100,29 @@ func (v *Client) Download(destdir string, url string) os.Error {
 		if err != nil {
 			return err
 		}
-		if bcount >= segments {
-			printf("#")
-			bcount = 0
-			continue
+		tdown += int64(read)
+		frac := float32(tdown) / float32(response.ContentLength)
+		percent := tdown * 100 / response.ContentLength
+		spercent := fmt.Sprintf("\r%v%% ", percent)
+		line := spercent
+		progress := int(frac*float32(winsize.Col)) - len(line) + 1
+		for i := 0; i < progress; i++ {
+			line += "#"
 		}
-		bcount += int64(read)
+		l := line
+		for i := 0; i < int(winsize.Col)-len(l); i++ {
+			line += " "
+		}
+		fprintf(os.Stderr, "%v", line)
+		go func() {
+			select {
+			case <-tick.C:
+				println("tick")
+			}
+		}()
 	}
+	tick.Stop()
 	println()
-	//_, err = io.Copy(f, response.Body)
 	return err
 }
 
--- pkg/gurl_test.go
+++ pkg/gurl_test.go
@@ -5,6 +5,9 @@ import (
 )
 
 func TestDownload(t *testing.T) {
+	//url := "http://github.com/downloads/str1ngs/gcc-go/gcc-go-snapshot-4.7.20110423-1-x86_64.pkg.tar.xz"
+	//url := "http://github.com/downloads/str1ngs/gcc-go/gcc-go-snapshot-4.7.20110423-1-x86_64.pkg.tar.xz"
+	//url  := "http://mirrors.kernel.org/archlinux/iso/latest/archlinux-2010.05-netinstall-i686.iso"
 	url := "http://localhost/gcc-go-snapshot-4.7.20110423-1-x86_64.pkg.tar.xz"
 	gurl := &Client{}
 	if err := gurl.Download("./", url); err != nil {
