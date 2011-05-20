d2a33e6357b1ab277cb7e78f61ad566ca821bd65
str1ngs <schizoid29@gmail.com>
2011-05-20 03:57:42 +0000

    use tick to throttle output.

--- pkg/gurl.go
+++ pkg/gurl.go
@@ -1,14 +1,13 @@
 package gurl
 
 import (
+	"bufio"
 	"fmt"
 	"http"
 	"os"
 	"path"
-	//term "github.com/kless/go-term/term"
-	"syscall"
+	term "github.com/kless/go-term/term"
 	"time"
-	"unsafe"
 )
 
 var (
@@ -16,41 +15,15 @@ var (
 	fprintf = fmt.Fprintf
 	println = fmt.Println
 	debug   = false
-)
-
-const (
-	TIOCGWINSZ = 0x5413
+	buf     = bufio.NewWriter(os.Stdout)
 )
 
 type Client struct {
-	client *http.Client
-}
-
-type winsize struct {
-	Row    uint16
-	Col    uint16
-	Xpixel uint16
-	Ypixel uint16
-}
-
-func getWinSize() (*winsize, os.Error) {
-	ws := new(winsize)
-	r1, _, errno := syscall.Syscall(syscall.SYS_IOCTL,
-		uintptr(syscall.Stdin),
-		uintptr(TIOCGWINSZ),
-		uintptr(unsafe.Pointer(ws)))
-
-	if int(r1) == -1 {
-		return nil, os.NewSyscallError("getWinSize", int(errno))
-	}
-	return ws, nil
+	client         *http.Client
+	progressHandle func(int64, int64, int64, chan int)
 }
 
 func (v *Client) Download(destdir string, url string) os.Error {
-	winsize, err := getWinSize()
-	if err != nil {
-		return err
-	}
 	if v.client == nil {
 		v.client = new(http.Client)
 	}
@@ -66,10 +39,13 @@ func (v *Client) Download(destdir string, url string) os.Error {
 		os.Stderr.Write(b)
 	}
 	response, err := v.client.Do(request)
-	defer response.Body.Close()
 	if err != nil {
 		return err
 	}
+	if response.StatusCode != 200 {
+		return os.NewError("Error status " + response.Status)
+	}
+	defer response.Body.Close()
 	if debug {
 		b, err := http.DumpResponse(response, false)
 		if err != nil {
@@ -80,60 +56,62 @@ func (v *Client) Download(destdir string, url string) os.Error {
 	fpath := path.Join(destdir, path.Base(url))
 	f, err := os.Create(fpath)
 	defer f.Close()
-	_ = response.ContentLength / int64(winsize.Col)
-	var tdown int64
+	var downloaded int64
 	start := time.Seconds()
-	//var lastTick int64
+	tick := time.Tick(1e09)
 	for {
 		b := make([]byte, 1024)
 		read, err := response.Body.Read(b)
 		if err != nil && err != os.EOF {
 			return err
 		}
+		downloaded += int64(read)
+		select {
+		case <-tick:
+			doProgress(start, downloaded, response.ContentLength, fpath)
+		default:
+		}
 		if err == os.EOF {
+			doProgress(start, downloaded, response.ContentLength, fpath)
 			break
 		}
 		_, err = f.Write(b[0:read])
 		if err != nil {
 			return err
 		}
-		time.Sleep(1e04)
-		tdown += int64(read)
-		var bps int64
-		tick := time.Seconds() - start
-		//if tick > lastTick {
-		//lastTick = tick
-		if tick != 0 {
-			bps = tdown / tick
-		}
-		//}
-		frac := float32(tdown) / float32(response.ContentLength)
-		percent := tdown * 100 / response.ContentLength
-		tail := fmt.Sprintf("] %vKB/s %v%% ", bps/1024, percent)
-		progress := int(frac*float32(winsize.Col)) - len(tail)
-		var line string
-		line += "["
-		for i := 0; i < progress; i++ {
-			line += "#"
-		}
-		//l := line
-		for i := 0; i < int(winsize.Col)-len(tail); i++ {
-			//line += " "
-		}
-		line += tail
-		if len(line) > int(winsize.Col) {
-			//println("line is ",len(line),"should be",int(winsize.Col))
-			//return os.NewError("output formatting error")
-			t := len(line) - int(winsize.Col)
-			line = line[t:]
-		}
-		//println(tdown/tick)
-		//if bps != 0 {
-		printf("\r%v", line)
-		//}
 	}
-	println()
+	fmt.Println()
 	return err
+}
+
+func doProgress(start, downloaded, totalDownload int64, file string) {
+	winsize, _ := term.GetWinsize()
+	var bps int64
+	tick := time.Seconds() - start
+	if tick != 0 {
+		bps = downloaded / tick
+	}
+	frac := float32(downloaded) / float32(totalDownload)
+	percent := downloaded * 100 / totalDownload
+	tail := fmt.Sprintf("] %vKB/s %v%% %v ", bps/1024, percent, file)
+	var line string
+	progress := int(frac * (float32(winsize.Col) - float32(len(tail)) - 1))
+	for i := 0; i < progress; i++ {
+		line += "#"
+	}
+	l := line
+	for i := 0; i < int(winsize.Col)-len(tail)-len(l)-1; i++ {
+		line += " "
+	}
+	line += tail
+	if len(line) > int(winsize.Col) {
+		println("line is ", len(line), "should be", int(winsize.Col))
+		os.Exit(0)
+		//t := len(line) - int(winsize.Col)
+		//line = line[t:]
+	}
+	fmt.Fprintf(buf, "\r[%v", line)
+	buf.Flush()
 }
 
 func buildRequest(method, url string) (*http.Request, os.Error) {
--- pkg/gurl_test.go
+++ pkg/gurl_test.go
@@ -1,16 +1,22 @@
 package gurl
 
 import (
+	"fmt"
 	"testing"
+	"time"
 )
 
 func TestDownload(t *testing.T) {
+	start := time.Seconds()
 	//url := "http://github.com/downloads/str1ngs/gcc-go/gcc-go-snapshot-4.7.20110423-1-x86_64.pkg.tar.xz"
-	//url := "http://github.com/downloads/str1ngs/gcc-go/gcc-go-snapshot-4.7.20110423-1-x86_64.pkg.tar.xz"
+	url := "http://github.com/downloads/str1ngs/gcc-go/gcc-go-snapshot-4.7.20110423-1-x86_64.pkg.tar.xz"
 	//url  := "http://mirrors.kernel.org/archlinux/iso/latest/archlinux-2010.05-netinstall-i686.iso"
-	url := "http://localhost/gcc-go-snapshot-4.7.20110423-1-x86_64.pkg.tar.xz"
-	gurl := &Client{}
+	//url := "http://localhost/archlinux-2010.05-netinstall-i686.iso"
+	//url := "http://localhost/gcc-go-snapshot-4.7.20110423-1-x86_64.pkg.tar.xz"
+	gurl := new(Client)
 	if err := gurl.Download("./", url); err != nil {
 		t.Errorf("Download : %v", err)
 	}
+	total := time.Seconds() - start
+	fmt.Println("Finished in", total)
 }
