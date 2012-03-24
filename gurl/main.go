395a8731ad541b3b99ba613a2f9c3a0622e53743
str1ngs <mike.rosset@gmail.com>
2012-03-24 08:57:37 -0700

    go get friendly, use smaller test files

--- gurl/main.go
+++ gurl/main.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-	"gurl"
+	"github.com/str1ngs/gurl"
 	"log"
 	"os"
 )
--- gurl/main_test.go
+++ gurl/main_test.go
@@ -7,7 +7,7 @@ import (
 )
 
 func TestMain(t *testing.T) {
-	url := "http://mirrors.us.kernel.org/gnu/bash/bash-4.2.tar.gz"
+	url := "http://mirrors.us.kernel.org/gnu/bash/bash-4.2.tar.gz.sig"
 	defer os.Remove(path.Base(url))
 	urls := []string{url, url}
 	os.Args = append(os.Args, urls...)
